cb03830dcebb49e4ab4d7c80e4d659ee93644ca4
Chayanin <arm.crazyplay@gmail.com>
2021-01-27 14:44:26 +0700

    add template

---
+++ src/App.js
@@ -0,0 +1,31 @@
+function App() {
+  return (
+    <div class="ml-2">
+      <h3 class="title is-3">Person List</h3>
+
+      {/* Please make me a component!!! */}
+      <table class="table is-bordered mb-3">
+        <tbody>
+          <tr>
+            <td>Name</td>
+            <td>Gender</td>
+            <td>Age</td>
+          </tr>
+          <tr>
+            <td>Bob</td>
+            <td>male</td>
+            <td>50</td>
+          </tr>
+        </tbody>
+      </table>
+
+      {/* Code me please!!! */}
+      <h3 class="title is-3">ID Counter</h3>
+      <p>YOUR ID HERE</p>
+      <button >+</button>
+      <button >-</button>
+    </div>
+  );
+}
+
+export default App;
