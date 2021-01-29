c50b2fc1b75229cc3b2d43a6321d928b648ae11c
ChayaninSuatap <arm.crazyplay@gmail.com>
2021-01-29 14:08:54 +0700

    provide data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
 function App() {
+  const data = [
+    {
+      name: "Bob",
+      gender: "male",
+      age: "50"
+    },
+    {
+      name: "Alice",
+      gender: "male",
+      age: "20"
+    }
+  ];
   return (
     <div class="ml-2">
       <h3 class="title is-3">Person List</h3>
@@ -22,8 +34,9 @@ function App() {
       {/* Code me please!!! */}
       <h3 class="title is-3">ID Counter</h3>
       <p>YOUR ID HERE</p>
-      <button >+</button>
-      <button >-</button>
+      <button>-</button>
+      <button>reset</button>
+      <button>+</button>
     </div>
   );
 }
